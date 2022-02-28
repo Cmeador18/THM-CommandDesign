b25d72bf1195da540af67622d703cbbec0f90942
murraypm <murraypm@email.sc.edu>
2022-02-27 23:43:52 -0500

    Implemented input handler

--- InputHandler.java
+++ InputHandler.java
@@ -1,15 +1,17 @@
 import java.util.HashMap;
 
-public class InputHandler{
-
+public class InputHandler {
     private HashMap<String, Command> commands;
 
-    public InputHandler(Player player)
-    {
-
+    public InputHandler(Player player) {
+        commands = new HashMap<String, Command>();
+        commands.put("jump", new JumpCommand(player));
+        commands.put("run", new RunCommand(player));
+        commands.put("fire", new FireCommand(player));
+        commands.put("quit", new QuitCommand(player));
     }
-    public void buttonPressed(String button)
-    {
-        
+
+    public void buttonPressed(String button) {
+        commands.get(button).execute();
     }
 }
